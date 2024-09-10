7ca690bd79bab753ebe3f535eb3d8f6ca5fb0c9a
Ethan Buck <ST10250745@vcconnect.edu.za>
2024-09-10 19:24:01 +0200

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROGPOESEM2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
